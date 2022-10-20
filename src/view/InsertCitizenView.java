9b59f97ce6a3ddda3bfef5909246d62ea3d1ddef
nnminh322 <nnminh322@gmail.com>
2022-10-20 18:14:42 +0700

    viewInsert

---
+++ src/view/InsertCitizenView.java
@@ -0,0 +1,104 @@
+package view;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import model.Citizen;
+
+/**
+ *
+ * @author sv_minhnn
+ */
+public class InsertCitizenView extends JFrame {
+    private Citizen ctzInsert;
+    private JButton save;    
+    private JButton clear;
+    private JButton back;
+
+    
+    public InsertCitizenView(){
+        this.ctzInsert = new Citizen();
+        this.init();
+        this.setVisible(true);
+    }
+    public void init(){
+        this.setTitle("Insert Cititzen");
+        this.setSize(400,600);
+        this.setLocationRelativeTo(null);
+        BorderLayout borderLayout = new BorderLayout();
+        this.setLayout(borderLayout);
+        ActionListener ac = new InsertCitizenListener(this);
+
+        
+        JPanel jPanel_label = new JPanel();
+        jPanel_label.setLayout(new GridLayout(8, 1));
+        JLabel HouseHoldBookNumber = new JLabel("HouseHoldBookNumber",JLabel.CENTER);        
+        JLabel CitizenID = new JLabel("CitizenID",JLabel.CENTER);
+        JLabel FullName = new JLabel("FullName",JLabel.CENTER);
+        JLabel HouseNumber = new JLabel("HouseNumber",JLabel.CENTER);
+        JLabel Street = new JLabel("Street",JLabel.CENTER);
+        JLabel Ward = new JLabel("Ward",JLabel.CENTER);
+        JLabel District = new JLabel("District",JLabel.CENTER);
+        JLabel DateofBirth = new JLabel("DateofBirth",JLabel.CENTER);
+        jPanel_label.add(HouseHoldBookNumber);        
+        jPanel_label.add(CitizenID);
+        jPanel_label.add(FullName);
+        jPanel_label.add(HouseNumber);
+        jPanel_label.add(Street);
+        jPanel_label.add(Ward);
+        jPanel_label.add(District);
+        jPanel_label.add(DateofBirth);
+
+        JPanel jPanel_textfield = new JPanel();
+        jPanel_textfield.setLayout(new GridLayout(8,1));
+        JTextField text1 = new JTextField();        
+        JTextField text2 = new JTextField();
+        JTextField text3 = new JTextField();
+        JTextField text4 = new JTextField();
+        JTextField text5 = new JTextField();
+        JTextField text6 = new JTextField();
+        JTextField text7 = new JTextField();
+        JTextField text8 = new JTextField();
+        jPanel_textfield.add(text1);
+        jPanel_textfield.add(text2);
+        jPanel_textfield.add(text3);
+        jPanel_textfield.add(text4);
+        jPanel_textfield.add(text5);
+        jPanel_textfield.add(text6);
+        jPanel_textfield.add(text7);
+        jPanel_textfield.add(text8);
+      
+        JPanel jPanel_control = new JPanel();
+        jPanel_control.setLayout(new FlowLayout());
+        save = new JButton("Save");
+        save.addActionListener(ac);
+        back = new JButton("Back");
+        clear = new JButton("Clear");
+        jPanel_control.add(back);
+        jPanel_control.add(clear);
+        jPanel_control.add(save);
+        
+        
+        
+        this.add(jPanel_label, BorderLayout.WEST);
+        this.add(jPanel_textfield,BorderLayout.CENTER);
+        this.add(jPanel_control, BorderLayout.SOUTH);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
